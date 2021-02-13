badc0fb886eb5a8a400f59a8197d31c3657df95f
Matheus H. Burey <65559844+Matheus-Henrique-Burey@users.noreply.github.com>
2021-02-12 22:08:19 -0300

    update

---
+++ Modulo-01/ex0012/ex0012.py
@@ -0,0 +1,3 @@
+produto = float(input('Digite o valor do produto: R$ '))
+desconto = produto - produto * 5 / 100
+print(f'O produto vai ficar de {produto:.2f} para {desconto:.2f}')
---
+++ Modulo-01/ex013/ex013.py
@@ -0,0 +1,3 @@
+salario = float(input('Valor do salario: R$ '))
+novo = salario + salario * 15 / 100
+print(f'O salario {salario:.2f} com 15% de almento ficou {novo:.2f}')
