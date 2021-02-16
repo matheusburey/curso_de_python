8aae18c2b02d0d90abefd418edd165c3d0d88170
Matheus H. Burey <65559844+Matheus-Henrique-Burey@users.noreply.github.com>
2021-02-16 17:54:34 -0300

    ex021

--- Modulo-01/ex016/ex016.py
+++ Modulo-01/ex016/ex016.py
@@ -1,3 +1,2 @@
-from math import floor
 num = float(input('Digite um numero para ver sua porçao inteira: '))
-print(f'O numero {num} tem a parte inteira {floor(num)}')
+print(f'O numero {num} tem a parte inteira {int(num)}')
