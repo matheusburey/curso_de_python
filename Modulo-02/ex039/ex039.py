e756ed7fdfb530b48322f37c9c6e39e97b31025b
Matheus H. Burey <65559844+Matheus-Henrique-Burey@users.noreply.github.com>
2021-03-03 21:14:15 -0300

    update

--- Modulo-02/ex039/ex039.py
+++ Modulo-02/ex039/ex039.py
@@ -1,13 +1,19 @@
 from datetime import date
-print('-' * 10)
+print('-' * 40)
 print('ALISTAMENTO DO EXERCITO BRASILEIRO')
-print('-' * 10)
+print('-' * 40)
 ano = int(input('Que ano você nasceu: '))
 atual = date.today().year
 idade = atual - ano
+
+print(f'Quem nasceu em {ano} tem {idade} anos em {atual}')
 if idade < 18:
-    print(f'Faltam {18 - idade} para voce se apresentar no exercito')
+   
+    print(f'Faltam {saldo} para voce se apresentar no exercito')
+    print(f'Seu alistameno sera no ano {atual + saldo}')
 elif idade == 18:
     print('voce tem que se apresentar')
 else:
-    print(f'Voce esta atrasou {idade - 18} anos para se apreentar')
+    saldo = idade - 18
+    print(f'Voce esta atrasou {saldo} anos para se apreentar')
+    print(f'Voce deveria ter se alistado no ano {atual - saldo}')
