f440b51484339f6cb749c8a02b132b4bbf2cadb1
Matheus H. Burey <65559844+Matheus-Henrique-Burey@users.noreply.github.com>
2021-03-11 22:33:50 -0300

    modulo3

---
+++ Modulo-03/ex072/ex072.py
@@ -0,0 +1,3 @@
+num = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezpito', 'dezenove', 'vinte')
+n = int(input('Digite um numero de 0 a 20: '))
+print(f'O numer digitado por extenso é {num[n]}')
