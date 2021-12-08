9cfbf6758293db8c8c2c94d95a0d57f62ffb38ea
Matheus-Henrique-Burey <matheushburei.99@gmail.com>
2021-12-08 14:40:35 -0300

    refatoração soma ímpares múltiplos de três

--- Modulo-02/ex048/ex048.py
+++ Modulo-02/ex048/ex048.py
@@ -1,6 +1,5 @@
 total = con = 0
-for c in range(1, 501 , 2):
-    if c % 3 == 0:
-        total += c
-        con += 1
+for c in range(3, 501, 6):
+    total += c
+    con += 1
 print(f'A soma de todos {con} os valores é {total}')
